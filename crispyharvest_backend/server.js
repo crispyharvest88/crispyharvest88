cc3f5d575d772190c7d8f30d0c1492dd175bbbc2
gabyybaraa <gabyy.pang@gmail.com>
2026-06-17 23:28:38 +0800

    Fix backend CORS for GitHub Pages

--- crispyharvest_backend/server.js
+++ crispyharvest_backend/server.js
@@ -11,7 +11,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://crispyharvest88.github.io",
+      "https://crispyharvest88.github.io/crispyharvest88",
+      "https://crispyharvest88.github.io/crispyharvest88/",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
