ea62938b04d1ed3bde5fee431b87c7f0af449412
gabyybaraa <gabyy.pang@gmail.com>
2026-06-17 22:15:17 +0800

    Connect frontend to deployed backend

--- src/App.jsx
+++ src/App.jsx
@@ -482,7 +482,7 @@ export default function App() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/send-reset-email", {
+      const response = await fetch("https://crispyharvest-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
